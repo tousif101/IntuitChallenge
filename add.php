c4ed3472908df3232607ab504e128961373abf6a
Winston <winstonwut@gmail.com>
2015-09-19 06:56:16 -0400

    add db thing

--- add.php
+++ add.php
@@ -7,3 +7,23 @@
  */
 
 $link = mysqli_connect("wkcrpi.student.rit.edu", "root", "Cakeisg00d", "intuit");
+
+$lastname = mysqli_real_escape_string($link, $_POST['lastname']);
+$firstname = mysqli_real_escape_string($link, $_POST['firstname']);
+$address = mysqli_real_escape_string($link, $_POST['address']);
+$marital = mysqli_real_escape_string($link, $_POST['marital']);
+$homeowner = mysqli_real_escape_string($link, $_POST['homeowner']);
+$childstatus = mysqli_real_escape_string($link, $_POST['childstatus']);
+$email = mysqli_real_escape_string($link, $_POST['email']);
+
+$sql = "INSERT INTO users (lastname, firstname, address, marital, homeowner, childstatus, email)
+VALUES ($lastname, $firstname, $address, $marital, $homeowner, $childstatus, $email)";
+
+if ($link->query($sql) == TRUE) {
+    echo "Successful";
+} else {
+    echo "Failed";
+}
+
+$link->close();
+
